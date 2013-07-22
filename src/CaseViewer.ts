4fb86e9e6cf9e46335162a352cf79475a827b03b
uchida <uchida.atsushi.8@gmail.com>
2013-07-23 03:57:29 +0900

    fix coding style

--- src/CaseViewer.ts
+++ src/CaseViewer.ts
@@ -5,57 +5,57 @@
 /* VIEW (MVC) */
 
 class HTMLDoc {
-    DocBase: JQuery;
-    Width: number;
-    Height: number;
+	DocBase: JQuery;
+	Width: number;
+	Height: number;
 
-    Render(Viewer: CaseViewer, CaseModel: CaseModel): void {
-        if (this.DocBase != null) {
-            var parent = this.DocBase.parent();
-            if(parent != null) parent.remove(this.DocBase);
-        }
-        this.DocBase = $('<div>').width(CaseViewer.ElementWidth);
-        this.DocBase.append($('<h4>' + CaseModel.Label + '</h4>'));
-        this.DocBase.append($('<p>' + CaseModel.Statement + '</p>'));
-        this.InvokePlugInRender(Viewer, CaseModel, this.DocBase);
-        // set height
-        this.Width = this.DocBase.width();
-        this.Height = this.DocBase.height();
-    }
+	Render(Viewer: CaseViewer, CaseModel: CaseModel): void {
+		if (this.DocBase != null) {
+			var parent = this.DocBase.parent();
+			if(parent != null) parent.remove(this.DocBase);
+		}
+		this.DocBase = $('<div>').width(CaseViewer.ElementWidth);
+		this.DocBase.append($('<h4>' + CaseModel.Label + '</h4>'));
+		this.DocBase.append($('<p>' + CaseModel.Statement + '</p>'));
+		this.InvokePlugInRender(Viewer, CaseModel, this.DocBase);
+		// set height
+		this.Width = this.DocBase.width();
+		this.Height = this.DocBase.height();
+	}
 
-    InvokePlugInRender(CaseViewer: CaseViewer, CaseModel: CaseModel, DocBase: JQuery): void {
-        for (var anno in CaseModel.Annotations) {
-            var f = CaseViewer.GetPlugInRender(anno.Name);
-            DocBase.append(f(CaseViewer, CaseModel, anno));
-        }
-        for (var note in CaseModel.Notes) {
-            var f = CaseViewer.GetPlugInRender(note.Name);
-            DocBase.append(f(CaseViewer, CaseModel, note));
-        }
-    }
+	InvokePlugInRender(CaseViewer: CaseViewer, CaseModel: CaseModel, DocBase: JQuery): void {
+		for (var anno in CaseModel.Annotations) {
+			var f = CaseViewer.GetPlugInRender(anno.Name);
+			DocBase.append(f(CaseViewer, CaseModel, anno));
+		}
+		for (var note in CaseModel.Notes) {
+			var f = CaseViewer.GetPlugInRender(note.Name);
+			DocBase.append(f(CaseViewer, CaseModel, note));
+		}
+	}
 
-    Resize(Viewer: CaseViewer, Source: CaseModel): void {
-        this.Width = this.DocBase ? this.DocBase.width() : 0;
-        this.Height = this.DocBase ? this.DocBase.height() : 0;
-    }
+	Resize(Viewer: CaseViewer, Source: CaseModel): void {
+		this.Width = this.DocBase ? this.DocBase.width() : 0;
+		this.Height = this.DocBase ? this.DocBase.height() : 0;
+	}
 }
 
 class SVGShape {
-    ParentView: ElementShape;
+	ParentView: ElementShape;
 	Width  :   number;
 	Height :   number;
 	Shape  : any;	
 
-	Resize (CaseViewer: CaseViewer, CaseModel: CaseModel, HTMLDoc: HTMLDoc) : void {
-        this.Width = HTMLDoc.Width;
-        this.Height = HTMLDoc.Height;
-	}
+	 Resize (CaseViewer: CaseViewer, CaseModel: CaseModel, HTMLDoc: HTMLDoc) : void {
+		 this.Width = HTMLDoc.Width;
+		 this.Height = HTMLDoc.Height;
+	 }
 
 }
 
 interface JQuery {
-    svg(loadUrl: string): JQuery;
-    svg(x: Function): JQuery;
+	svg(loadUrl: string): JQuery;
+	svg(x: Function): JQuery;
 }
 
 class ElementShape {
@@ -66,35 +66,34 @@ class ElementShape {
 
 	AbsX : number;  //
 	AbsY : number;
-    x: number;
-    y: number;
-	
+	x: number;
+	y: number;
+
 	constructor(CaseViewer : CaseViewer, CaseModel : CaseModel) {
 		this.CaseViewer = CaseViewer;
-        this.Source = CaseModel;
-        this.HTMLDoc = new HTMLDoc();
-        this.HTMLDoc.Render(CaseViewer, CaseModel);
+		this.Source = CaseModel;
+		this.HTMLDoc = new HTMLDoc();
+		this.HTMLDoc.Render(CaseViewer, CaseModel);
 		this.SVGShape = new SVGShape();
 	}
-	
+
 	Resize() : void {
-        this.HTMLDoc.Resize(this.CaseViewer, this.Source);
-        this.SVGShape.Resize(this.CaseViewer, this.Source, this.HTMLDoc);
+		this.HTMLDoc.Resize(this.CaseViewer, this.Source);
+		this.SVGShape.Resize(this.CaseViewer, this.Source, this.HTMLDoc);
 	}
 
 	AppendHTMLElement(root : JQuery) : void {
-        // Tetsurom
-        // if it has an parent, add an arrow element. 
-        //svg.rect(parent, this.AbsX, this.AbsY, this.HTMLDoc.Width, this.HTMLDoc.Height);
-        var rect = $(document.createElementNS('http://www.w3.org/2000/svg', "rect")).attr({
-            fill: "none", stroke: "gray",
-            x: this.AbsX, y: this.AbsY, width: this.HTMLDoc.Width, height: this.HTMLDoc.Height,
-            //points: "0,0 0,0 0,0 0,0"
-        });
-        rect.appendTo(root);
+		// Tetsurom
+		// if it has an parent, add an arrow element. 
+		//svg.rect(parent, this.AbsX, this.AbsY, this.HTMLDoc.Width, this.HTMLDoc.Height);
+		var rect = $(document.createElementNS('http://www.w3.org/2000/svg', "rect")).attr({
+			fill: "none", stroke: "gray",
+			x: this.AbsX, y: this.AbsY, width: this.HTMLDoc.Width, height: this.HTMLDoc.Height,
+			//points: "0,0 0,0 0,0 0,0"
+		});
+		rect.appendTo(root);
 		return; // TODO
 	}
-	
 }
 
 class CaseViewerConfig {
@@ -104,22 +103,22 @@ class CaseViewerConfig {
 var ViewerConfig = new CaseViewerConfig();
 
 class CaseViewer {
-    ViewMap: { [index: string]: ElementShape; }
+	ViewMap: { [index: string]: ElementShape; };
 
-    static ElementWidth = 150;
-	
-	constructor(Source : CaseModel) {
-        this.ViewMap = <any>[]; // a hack to avoid tsc's problem.
+	 static ElementWidth = 150;
+
+	 constructor(Source : CaseModel) {
+		this.ViewMap = <any>[]; // a hack to avoid tsc's problem.
 		for(var model in Source) {
-            this.ViewMap[model.Label] = new ElementShape(this, model);
-        }
-		this.Resize();
-    }
+			this.ViewMap[model.Label] = new ElementShape(this, model);
+		}
+			this.Resize();
+	}
 
-    GetPlugInRender(Name: string): (CaseViewer: CaseViewer, CaseModel: CaseModel, Node: string) => string {
-        return null; // TODO;
-    }
-	
+	GetPlugInRender(Name: string): (CaseViewer: CaseViewer, CaseModel: CaseModel, Node: string) => string {
+		return null; // TODO;
+	}
+
 	Resize() : void {
 		for(var shape in this.ViewMap) {
 			this.ViewMap[shape].Resize();
@@ -131,36 +130,36 @@ class CaseViewer {
 		// TODO: ishii
 	}
 
-	Draw(svg : JQuery) : void {
-		for(var shape in this.ViewMap) {
-            this.ViewMap[shape].AppendHTMLElement(svg);
+		Draw(svg : JQuery) : void {
+			for(var shape in this.ViewMap) {
+				this.ViewMap[shape].AppendHTMLElement(svg);
+			}
 		}
-	}
 
 }
 
 class ServerApi {
-    constructor(url: string) {
-    }
-    GetCase(project: string, id: string): string {
-        return "[]";
-    }
+	constructor(url: string) {
+	}
+	GetCase(project: string, id: string): string {
+		return "[]";
+	}
 }
 
 function StartCaseViewer(url : string, id : string) {
-    var loader = new ServerApi(url);
-    var project; // temp
+	var loader = new ServerApi(url);
+	var project; // temp
 	var JsonData = loader.GetCase(project, id);
-    var Argument = new Argument();
-    var model = new CaseDecoder().ParseJson(Argument, JsonData);
+	var Argument = new Argument();
+	var model = new CaseDecoder().ParseJson(Argument, JsonData);
 	var CaseViewer = new CaseViewer(model);
 	var svg = document.getElementById(id);
 	CaseViewer.Draw(svg);
 }
 
 $(function () {
-    var model = new CaseModel(new Case(), null, CaseType.Goal, "G0", "Top Goal");
-    var Viewer = new CaseViewer(model);
-    var root: any = $("#svgroot");
-    Viewer.Draw(root);
+	var model = new CaseModel(new Case(), null, CaseType.Goal, "G0", "Top Goal");
+	var Viewer = new CaseViewer(model);
+	var root: any = $("#svgroot");
+	Viewer.Draw(root);
 });
