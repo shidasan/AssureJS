5639a1c3b201406686befba2af73b86e2ddf3385
yuki <yuki@yuki-VirtualBox.(none)>
2013-07-22 20:58:18 +0900

    imple JsonParser

--- src/CaseDecoder.ts
+++ src/CaseDecoder.ts
@@ -6,14 +6,63 @@ function OutputError(o : any) : void {
 
 class Parser {
 
-	Argument : Argument;
+	Case : Case;
 
-	constructor(Argument : Argument) {
-		this.Argument = Argument;
+	constructor(Case : Case) {
+		this.Case = Case;
 	}
 
 	parse(source : any) : CaseModel {
 		return null;
+	}
+
+}
+
+class JsonParser extends Parser {
+
+	caseModelMap : any = {};
+
+	initCaseModelMap(NodeList : any[] /* TODO: remove any type */) : void {
+		for(var i : number = 0; i < NodeList.length; i++) {
+			this.caseModelMap[NodeList[i]["Label"]] = NodeList[i];
+		}
+	}
+
+	parseChild(childLabel : string, Parent : CaseModel) : CaseModel {
+		var caseModelData : any = this.caseModelMap[childLabel]; // TODO: remove any type
+		var Type : CaseType = caseModelData["NodeType"]; // fix NodeType's type
+		var Statement : string = caseModelData["Statement"];
+		var Children : string[] = caseModelData["Children"];
+		var Notes : CaseNote[] = caseModelData["Notes"];
+		var Annotations : CaseAnnotation[] = caseModelData["Annotations"];
+
+		var childCaseModel : CaseModel = new CaseModel(this.Case, Parent, Type, childLabel, Statement);
+
+		for(var i : number = 0; i < Children.length; i++) {
+			this.parseChild(Children[i], childCaseModel);
+		}
+
+		if(Parent == null) {
+			return childCaseModel;
+		}
+		else {
+			Parent.Children.push(childCaseModel);
+			return parentNode;
+		}
+	}
+
+	parse(source : any) : CaseModel {
+		var JsonData : any = <any>source; // TODO: remove any type
+		var DCaseName : string = JsonData["DCaseName"]; // Is it necessary?
+		var NodeCount : number = JsonData["NodeCount"]; // Is it necessary?
+		var TopGoalLabel : string = JsonData["TopGoalLabel"]; // Is it necessary?
+		var NodeList : any[] = JsonData["NodeList"]; // TODO: remove any type
+
+		this.initCaseModelMap(NodeList);
+
+		var root : CaseModel = this.parseChild(TopGoalLabel, null);
+
+		return root;
 	}
 
 }
@@ -32,20 +81,20 @@ class CaseDecoder {
 	constructor() {
 	}
 
-	ParseJson(Argument : Argument, jsonData : any) : CaseModel  {
+	ParseJson(Case : Case, JsonData : any) : CaseModel  {
+		var JsonParser : JsonParser = new JsonParser(Case);
+		var root : CaseModel = JsonParser.parse(JsonData);
+		return root;
+	}
+
+	ParseDCaseXML(Case : Case, XML : any) : CaseModel {
 		// TODO
 		return null;
 	}
 
-	ParseDCaseXML(Argument : Argument, XML : any) : CaseModel {
-		// TODO
-		return null;
-	}
-
-	ParseMarkCase(Argument : Argument,  MarkCase: string) : CaseModel {
+	ParseMarkCase(Case : Case,  MarkCase: string) : CaseModel {
 		// TODO
 		return null;
 	}
 
 }
-
