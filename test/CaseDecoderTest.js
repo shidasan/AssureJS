6d1f7df9f7104dc60e7d6fe252595cd252d108fd
Okamoto Yuki <yuki@Okamotos-MacBook-Pro.local>
2013-07-24 03:52:20 +0900

    add CaseDecoderTest.ts

--- test/CaseDecoderTest.js
+++ test/CaseDecoderTest.js
@@ -1,94 +1,99 @@
+/// <reference path="../d.ts/qunit.d.ts" />
+/// <reference path="../src/CaseDecoder.ts" />
 function checkCaseModel(root) {
-	var ret = true;
-	ret &= ("G1" == root.Label);
+    if ("G1" != root.Label)
+        return false;
 
-	var child = root.Children[0];
-	ret &= ("S1" == child.Label);
+    var child = root.Children[0];
+    if ("S1" != child.Label)
+        return false;
 
-	var grandchildren = child.Children;
-	ret &= (grandchildren.length == 2);
+    var grandchildren = child.Children;
+    if (grandchildren.length != 2)
+        return false;
 
-	for(var i = 0; i < grandchildren.length; i++) {
-		if(grandchildren[i].Label == "G2") {
-			ret &= ("E1" == grandchildren[i].Children[0].Label);
-		}
-		else if(grandchildren[i].Label == "G3") {
-			ret &= ("E2" == grandchildren[i].Children[0].Label);
-		}
-	}
+    for (var i = 0; i < grandchildren.length; i++) {
+        if (grandchildren[i].Label == "G2") {
+            if ("E1" != grandchildren[i].Children[0].Label)
+                return false;
+        } else if (grandchildren[i].Label == "G3") {
+            if ("E2" != grandchildren[i].Children[0].Label)
+                return false;
+        }
+    }
 
-	return ret;
+    return true;
 }
 
-test("CaseDecoderTest", function() {
-	var JsonData = {
-			"DCaseName" : "test",
-			"NodeCount" : 6,
-			"TopGoalLabel" : "G1",
-			"NodeList": [
-				{
-					"Children": [
-						"S1"
-					],
-					"Statement": "",
-					"NodeType": 0,
-					"Label": "G1",
-					"Annotations" : [],
-					"Notes" : []
-				},
-				{
-					"Children": [
-						"G2",
-						"G3"
-					],
-					"Statement": "",
-					"NodeType": 2,
-					"Label": "S1",
-					"Annotations" : [],
-					"Notes" : []
-				},
-				{
-					"Children": [
-						"E1",
-					],
-					"Statement": "",
-					"NodeType": 0,
-					"Label": "G2",
-					"Annotations" : [],
-					"Notes" : []
-				},
-				{
-					"Children": [
-						"E2",
-					],
-					"Statement": "",
-					"NodeType": 0,
-					"Label": "G3",
-					"Annotations" : [],
-					"Notes" : []
-				},
-				{
-					"Children": [],
-					"Statement": "",
-					"NodeType": 3,
-					"Label": "E1",
-					"Annotations" : [],
-					"Notes" : []
-				},
-				{
-					"Children": [],
-					"Statement": "",
-					"NodeType": 3,
-					"Label": "E2",
-					"Annotations" : [],
-					"Notes" : []
-				},
-			]
-	}
+test("CaseDecoderTest", function (assert) {
+    var JsonData = {
+        "DCaseName": "test",
+        "NodeCount": 6,
+        "TopGoalLabel": "G1",
+        "NodeList": [
+            {
+                "Children": [
+                    "S1"
+                ],
+                "Statement": "",
+                "NodeType": 0,
+                "Label": "G1",
+                "Annotations": [],
+                "Notes": []
+            },
+            {
+                "Children": [
+                    "G2",
+                    "G3"
+                ],
+                "Statement": "",
+                "NodeType": 2,
+                "Label": "S1",
+                "Annotations": [],
+                "Notes": []
+            },
+            {
+                "Children": [
+                    "E1"
+                ],
+                "Statement": "",
+                "NodeType": 0,
+                "Label": "G2",
+                "Annotations": [],
+                "Notes": []
+            },
+            {
+                "Children": [
+                    "E2"
+                ],
+                "Statement": "",
+                "NodeType": 0,
+                "Label": "G3",
+                "Annotations": [],
+                "Notes": []
+            },
+            {
+                "Children": [],
+                "Statement": "",
+                "NodeType": 3,
+                "Label": "E1",
+                "Annotations": [],
+                "Notes": []
+            },
+            {
+                "Children": [],
+                "Statement": "",
+                "NodeType": 3,
+                "Label": "E2",
+                "Annotations": [],
+                "Notes": []
+            }
+        ]
+    };
 
-	var testCase = new Case();
-	var caseDecoder = new CaseDecoder();
-	var root = caseDecoder.ParseJson(testCase, JsonData);
+    var testCase = new Case();
+    var caseDecoder = new CaseDecoder();
+    var root = caseDecoder.ParseJson(testCase, JsonData);
 
-	ok(checkCaseModel(root), "Created CaseModel is correct");
+    ok(checkCaseModel(root), "Created CaseModel is correct");
 });
---
+++ test/CaseDecoderTest.ts
@@ -0,0 +1,96 @@
+/// <reference path="../d.ts/qunit.d.ts" />
+/// <reference path="../src/CaseDecoder.ts" />
+
+function checkCaseModel(root : CaseModel) : boolean {
+	if("G1" != root.Label) return false;
+
+	var child = root.Children[0];
+	if("S1" != child.Label) return false;
+
+	var grandchildren = child.Children;
+	if(grandchildren.length != 2) return false;
+
+	for(var i = 0; i < grandchildren.length; i++) {
+		if(grandchildren[i].Label == "G2") {
+			if("E1" != grandchildren[i].Children[0].Label) return false;
+		}
+		else if(grandchildren[i].Label == "G3") {
+			if("E2" != grandchildren[i].Children[0].Label) return false;
+		}
+	}
+
+	return true;
+}
+
+test("CaseDecoderTest", (assert? : QUnitAssert) : any => {
+	var JsonData : any = {
+			"DCaseName" : "test",
+			"NodeCount" : 6,
+			"TopGoalLabel" : "G1",
+			"NodeList": [
+				{
+					"Children": [
+						"S1"
+					],
+					"Statement": "",
+					"NodeType": 0,
+					"Label": "G1",
+					"Annotations" : [],
+					"Notes" : []
+				},
+				{
+					"Children": [
+						"G2",
+						"G3"
+					],
+					"Statement": "",
+					"NodeType": 2,
+					"Label": "S1",
+					"Annotations" : [],
+					"Notes" : []
+				},
+				{
+					"Children": [
+						"E1",
+					],
+					"Statement": "",
+					"NodeType": 0,
+					"Label": "G2",
+					"Annotations" : [],
+					"Notes" : []
+				},
+				{
+					"Children": [
+						"E2",
+					],
+					"Statement": "",
+					"NodeType": 0,
+					"Label": "G3",
+					"Annotations" : [],
+					"Notes" : []
+				},
+				{
+					"Children": [],
+					"Statement": "",
+					"NodeType": 3,
+					"Label": "E1",
+					"Annotations" : [],
+					"Notes" : []
+				},
+				{
+					"Children": [],
+					"Statement": "",
+					"NodeType": 3,
+					"Label": "E2",
+					"Annotations" : [],
+					"Notes" : []
+				},
+			]
+	}
+
+	var testCase : Case = new Case();
+	var caseDecoder : CaseDecoder = new CaseDecoder();
+	var root : CaseModel = caseDecoder.ParseJson(testCase, JsonData);
+
+	ok(checkCaseModel(root), "Created CaseModel is correct");
+});
