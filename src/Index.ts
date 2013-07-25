5fec9fe045d449e4735c339ccc56591b69b94c7d
masakiishii <masaki.ishii511@gmail.com>
2013-07-24 23:41:07 -0700

    revert src/Index.ts

--- src/Index.ts
+++ src/Index.ts
@@ -11,7 +11,7 @@ $(function () {
 
 	var JsonData = {
 		"DCaseName": "test",
-		"NodeCount": 17,
+		"NodeCount": 7,
 		"TopGoalLabel": "G1",
 		"NodeList": [
 			{
@@ -47,7 +47,7 @@ $(function () {
 			},
 			{
 				"Children": [
-					"S2"
+					"E1"
 				],
 				"Statement": "",
 				"NodeType": 0,
@@ -57,44 +57,11 @@ $(function () {
 			},
 			{
 				"Children": [
-					"G4",
-					"G5",
-					"G6"
-				],
-				"Statement": "",
-				"NodeType": 2,
-				"Label": "S2",
-				"Annotations": [],
-				"Notes": []
-			},
-			{
-				"Children": [
-					"E1"
+					"E2"
 				],
 				"Statement": "",
 				"NodeType": 0,
-				"Label": "G4",
-				"Annotations": [],
-				"Notes": []
-			},
-			{
-				"Children": [
-					"E2",
-					"E3"
-				],
-				"Statement": "",
-				"NodeType": 0,
-				"Label": "G5",
-				"Annotations": [],
-				"Notes": []
-			},
-			{
-				"Children": [
-					"E4"
-				],
-				"Statement": "",
-				"NodeType": 0,
-				"Label": "G6",
+				"Label": "G3",
 				"Annotations": [],
 				"Notes": []
 			},
@@ -113,73 +80,6 @@ $(function () {
 				"Statement": "",
 				"NodeType": 3,
 				"Label": "E2",
-				"Annotations": [],
-				"Notes": []
-			},
-			{
-				"Children": [
-				],
-				"Statement": "",
-				"NodeType": 3,
-				"Label": "E3",
-				"Annotations": [],
-				"Notes": []
-			},
-			{
-				"Children": [
-				],
-				"Statement": "",
-				"NodeType": 3,
-				"Label": "E4",
-				"Annotations": [],
-				"Notes": []
-			},
-			{
-				"Children": [
-					"S3"
-				],
-				"Statement": "",
-				"NodeType": 0,
-				"Label": "G3",
-				"Annotations": [],
-				"Notes": []
-			},
-			{
-				"Children": [
-					"G7",
-					"G8",
-					"G9"
-				],
-				"Statement": "",
-				"NodeType": 2,
-				"Label": "S3",
-				"Annotations": [],
-				"Notes": []
-			},
-			{
-				"Children": [
-				],
-				"Statement": "",
-				"NodeType": 0,
-				"Label": "G7",
-				"Annotations": [],
-				"Notes": []
-			},
-			{
-				"Children": [
-				],
-				"Statement": "",
-				"NodeType": 0,
-				"Label": "G8",
-				"Annotations": [],
-				"Notes": []
-			},
-			{
-				"Children": [
-				],
-				"Statement": "",
-				"NodeType": 0,
-				"Label": "G9",
 				"Annotations": [],
 				"Notes": []
 			},
